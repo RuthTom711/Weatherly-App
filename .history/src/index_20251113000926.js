291de8dd1ac2656fa3dbef7e260120a7aa0d4025
Ruth Tom <hlengiwetom@gmail.com>
2025-11-13 00:10:19 +0200

    added icon image

--- .history/src/index_20251113000926.js
+++ .history/src/index_20251113000926.js
@@ -7,11 +7,12 @@ function refreshWeather(response){
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
-    let iconElement= document.querySelector("#icon")  
+    let iconElement= document.querySelector("#icon")    
 
 
    
     cityElement.innerHTML = response.data.city;
+
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
